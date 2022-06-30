34d53499999d941a8b6e12cdfced3604d404e3c0
谢永诚 <752106133@qq.com>
2022-06-30 17:53:25 +0800

    bug fixed

--- src/main/java/com/merrycodes/Main.java
+++ src/main/java/com/merrycodes/Main.java
@@ -2,11 +2,10 @@ package com.merrycodes;
 
 import com.microsoft.graph.auth.enums.NationalCloud;
 import com.microsoft.graph.auth.publicClient.UsernamePasswordProvider;
-import com.microsoft.graph.models.extensions.IGraphServiceClient;
-import com.microsoft.graph.models.extensions.Message;
-import com.microsoft.graph.models.extensions.User;
-import com.microsoft.graph.requests.extensions.GraphServiceClient;
-import com.microsoft.graph.requests.extensions.IMessageCollectionPage;
+import com.microsoft.graph.models.Message;
+import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.requests.MessageCollectionPage;
+import okhttp3.Request;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -31,14 +30,16 @@ public class Main {
     public static void main(String[] args) {
         UsernamePasswordProvider authProvider = new UsernamePasswordProvider(args[0], Collections.singletonList("https://graph.microsoft.com/.default"),
                 args[1], args[2], NationalCloud.Global, args[3], args[4]);
-        IGraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider(authProvider).buildClient();
+        GraphServiceClient<Request> graphClient = GraphServiceClient
+          .builder().authenticationProvider(authProvider).buildClient();
         // 参数错误的时候会报错，而且也获取不了，jar包里面自己处理了(e.printStackTrace())
         // 获取用户
-        User user = graphClient.me().buildRequest().get();
+        //  User user = graphClient.me().buildRequest().get();
         // 查看用户邮件列表
-        IMessageCollectionPage iMessageCollectionPage = graphClient.users(user.userPrincipalName).messages().buildRequest().select("sender,subject").get();
+        MessageCollectionPage iMessageCollectionPage = graphClient.me().messages().buildRequest().select("sender,subject").get();
+        assert iMessageCollectionPage != null;
         List<Message> messageList = iMessageCollectionPage.getCurrentPage();
-        System.out.println(String.format("运行时间：%s —— 共有%d封件邮件", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), messageList.size()));
+        System.out.printf("运行时间：%s —— 共有%d封件邮件%n", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), messageList.size());
     }
 
 }
